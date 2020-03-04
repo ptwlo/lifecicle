14b9b83906204d72b5bb087f1fd12bd3a98b630f
Petter Lopez <petterlopez003@gmail.com>
2020-03-03 19:54:00 -0500

    second commit

---
+++ lifecicle/src/App.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state ={
+      items: [],
+      isLoaded: false,
+    }
+  }
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users").then(res =>{
+      return res.json();
+    }).then(data =>{
+      console.log(data);
+      this.setState({
+        isLoaded: true,
+        items: data,
+      });
+    });
+  }
+  render(){
+    var { isLoaded, items } = this.state;
+    if (!isLoaded){
+      return <div>Is loading....</div>;
+    }else{
+      return(
+        <>
+        <div clasName="App">
+          <div className="Names">
+            <ul>
+              {items.map(el=>{
+                return (
+                <li key={el.id}>Name: {el.name} | UserName: {el.username} | {' '}<a href={`https://${el.website}`}>Website</a></li>
+                );
+              })}
+            </ul>
+          </div>
+  
+        </div>
+        </>
+      )
+    }
+    
+  }
+}
+export default App;
